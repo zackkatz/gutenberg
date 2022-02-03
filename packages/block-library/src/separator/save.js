a60e15c6f10308780db87e9dd07ae3582fdcee6b
Glen Davies <glen.davies@a8c.com>
2022-02-03 17:01:04 +1300

    Update save method to cover all the existing use cases

--- packages/block-library/src/separator/save.js
+++ packages/block-library/src/separator/save.js
@@ -1,10 +1,31 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { getColorClassName, useBlockProps } from '@wordpress/block-editor';
 
 export default function separatorSave( { attributes } ) {
-	const { className } = attributes;
+	const { backgroundColor, style } = attributes;
+	const customColor = style?.color?.background;
 
-	return <hr { ...useBlockProps.save( { className } ) } />;
+	// The hr support changing color using border-color, since border-color
+	// is not yet supported in the color palette, we use background-color.
+
+	// The dots styles uses text for the dots, to change those dots color is
+	// using color, not backgroundColor.
+	const colorClass = getColorClassName( 'color', backgroundColor );
+
+	const className = classnames( {
+		'has-text-color': backgroundColor || customColor,
+		[ colorClass ]: colorClass,
+	} );
+
+	const styles = {
+		color: colorClass ? undefined : customColor,
+	};
+	return <hr { ...useBlockProps.save( { className, style: styles } ) } />;
 }
